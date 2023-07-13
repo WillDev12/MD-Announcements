5c5a381f5d6b41b4261a6aec6ac0d5947cc47228
WillDev12 <willdevv12@gmail.com>
2023-07-13 19:00:54 -0400

    Made it look a bit more professional

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/main.html");
+  res.redirect('https://github.com/WillDev12/MD-Announcements');
 });
 
 app.get("/api", (req, res) => {
